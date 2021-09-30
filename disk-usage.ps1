fedd5d31d4f312f0492e80ab1f6f40426ac81295
mdkadar <81559793+mdkadar@users.noreply.github.com>
2021-09-30 12:44:12 +0530

    Update disk-usage.ps1

--- disk-usage.ps1
+++ disk-usage.ps1
@@ -1,5 +1,7 @@
 $arg = Get-WmiObject Win32_Product -Filter "Name like 'Octopus Deploy Server'" | Select-Object -ExpandProperty Version
-If($arg -ne "2021.1.7665") {
+$versions = Invoke-RestMethod "https://octopus.com/download/upgrade/v3"
+$upgradeVersion = $versions[-1].Version
+If($arg -ne $upgradeVersion) {
 Write-Host "Not upto date.";
 } else {
 Write-Host "upto date."
